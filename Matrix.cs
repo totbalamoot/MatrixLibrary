258270b731e979e30002a54bd3584f6926f68ac0
totbalamoot <53047125+totbalamoot@users.noreply.github.com>
2019-07-24 17:44:44 +0300

    Добавлены конструкторы матриц и базовые операции над ними
    *Добавлено несколько конструкторов
    *Добавлены сложение, вычитание, умножение, транспонирование
    *Добавлен индексатор для класса

---
+++ Matrix.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    public class Matrix
+    {
+        private double[,] M;
+        private int n, m;
+
+        public int lengthN
+        {
+            get { return n; }            
+        }
+        public int lengthM
+        {
+            get { return m; }
+        }
+
+        public Matrix()
+        {
+            M = new double[1, 1];
+
+            M[0, 0] = 0;
+            n = 0;
+            m = 0;
+        }
+
+        public Matrix(int _n, int _m)
+        {            
+            n = _n;
+            m = _m;
+            M = new double[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    M[i, j] = 0;
+                }
+            }
+        }
+
+        public Matrix(int _n)
+        {
+            n = m = _n;
+            new Matrix(n, n);
+        }
+
+        public Matrix(double[,] Matr)
+        {
+            n = Matr.GetLength(0);
+            m = Matr.GetLength(1);
+            M = new double[n, m];
+
+            M = Matr;
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return M[i, j];
+            }
+            set
+            {
+                M[i, j] = value;
+            }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(String.Format("{0, 3:G4}", M[i, j]));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsEqualSize(Matrix B)
+        {
+            return (n == B.n) && (m == B.m);
+        }
+
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            if (A.IsEqualSize(B))
+            {
+                Matrix C = new Matrix(A.n, A.m);
+
+                for (int i = 0; i < A.n; i++)
+                {
+                    for (int j = 0; j < A.m; j++)
+                    {
+                        C[i, j] = A[i, j] + B[i, j];
+                    }
+                }
+
+                return C;
+            }
+            return new Matrix();
+        }
+
+        public static Matrix operator +(Matrix A, double b)
+        {
+            Matrix C = new Matrix(A.n, A.m);
+
+            for (int i = 0; i < A.n; i++)
+            {
+                for (int j = 0; j < A.m; j++)
+                {
+                    C[i, j] = A[i, j] + b;
+                }
+            }
+
+            return C;
+        }
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            if (A.IsEqualSize(B))
+            {
+                Matrix C = new Matrix(A.n, A.m);
+
+                for (int i = 0; i < A.n; i++)
+                {
+                    for (int j = 0; j < A.m; j++)
+                    { 
+                        C[i, j] = A[i, j] - B[i, j];
+                    }
+                }
+
+                return C;
+            }
+            return new Matrix();
+        }
+
+        public static Matrix operator *(double a, Matrix A)
+        {
+            for (int i = 0; i < A.n; i++)
+            {
+                for (int j = 0; j < A.m; j++)
+                {
+                    A[i, j] = a * A[i, j];
+                }
+            }
+
+            return A;
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            int l = A.n;
+            int m = A.m;
+            int n = B.m;
+
+            if ((A.m == B.n))
+            {
+                Matrix C = new Matrix(l, n);
+
+                for (int i = 0; i < l; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < m; k++)
+                        {
+                            C[i, j] += A[i, k] * B[k, j];
+                        }
+                    }
+                } 
+
+                return C;
+            }
+            return new Matrix();
+        }
+
+        public static Matrix Transpose(Matrix A)
+        {
+            int n = A.n;
+            int m = A.m;
+            Matrix B = new Matrix(m, n);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    B[j, i] = A[i, j];
+                }
+            }
+
+            return B;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(2);
+            double[,] matr = new double[2, 5];
+            Matrix matrix1 = new Matrix(matr);
+
+            Console.WriteLine(matrix.lengthN);
+            Console.WriteLine(matrix.lengthM);          
+            Console.WriteLine();
+
+            Console.WriteLine(matrix1.lengthN);
+            Console.WriteLine(matrix1.lengthM);
+            Console.WriteLine();
+
+            double[,] matr1 = new double[2, 2];
+            double[,] matr2 = new double[2, 2];
+            matr1 = new double[,] { { 1, 2 }, { 3, 4 } };
+            matr2 = new double[,] { { 4, 3 }, { 2, 1 } };
+            Matrix matrixA = new Matrix(matr1);
+            Matrix matrixB = new Matrix(matr2);
+            Matrix matrixC = matrixA - matrixB;
+
+            matrixC.Show();
+
+            Console.WriteLine();
+            matrixA.Show();
+            Console.WriteLine();
+            matrixA = Matrix.Transpose(matrixA);
+            matrixA.Show();
+        }
+    }
+}
